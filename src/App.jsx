1a496f4a3e2e1040b4bc7c91b2d10516487b137a
araujosamantha15 <araujosamantha15@gmail.com>
2025-05-09 02:27:50 +0000

    criação do arquivo cardfinal

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import illustrationThankyou from "./assets/illustration-thank-you.svg"
+
 import { useState } from "react"
 import { Card1 } from "./components/Card1";
 
@@ -14,16 +14,7 @@ export function App() {
     submited === false ? (
       <Card1 nota={nota} setNota={setNota} setSubmited={setSubmited}/>
     ) : (
-      <div className=" max-w-103 bg-gradient-dark p-6 rounded-2xl text-white font-overpass text-center">
-
-        <img className="mx-auto mb-6 "  src={illustrationThankyou} alt="" />
-
-        <p className="mb-6 text-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-3xl text-sm ">Você selecionou {nota} de 5</p>
-
-        <h1 className="text-2xl font-bold mb-2.5">Obrigada!</h1>
-
-        <p className="text-sm text-light-grey mb-6">Agradecemos por dedicar um momento para nos avaliar. Se precisar de mais suporte, não hesite em entrar em contato!</p>
-      </div>
+      <CardF nota={nota}/>
     )
 
   )
